54f72bf0adf1edc8f75105b695d7a9a095288b20
xime233 <ximena233laur@gmail.com>
2026-06-02 13:37:06 -0300

    fix railway postgres connection using env vars

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: process.env.PGPORT,
   ssl: {
     rejectUnauthorized: false
   }
