427c00473aaeaa9375a6c2dad98119daf5aae82c
xime233 <ximena233laur@gmail.com>
2026-08-05 18:01:30 -0300

    Agrego endpoint fulfillment y soporte para Render

--- server.js
+++ server.js
@@ -246,6 +246,8 @@ app.post("/fulfillment", async (req, res) => {
 
   }
 }); 
-app.listen(3000, () => {
-  console.log("Servidor iniciado en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor iniciado en puerto ${PORT}`);
 });
